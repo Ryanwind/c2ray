3b13061e25a62893f90174d898af3eb537f2edd6
yude268 <yude268@gmail.com>
2019-06-20 23:47:35 +0800

    Signed-off-by: yude268 <yude268@gmail.com>

---
+++ C2rayPro/app/系统代理.bat
@@ -0,0 +1,68 @@
+@echo off
+
+::获取管理员权限
+%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
+cd /d "%~dp0"
+
+::命令行标题栏和文字颜色
+title -- 添加-启动开关-到桌面右键 --
+MODE con: COLS=43 lines=11
+color 0a
+
+
+
+::主菜单
+:begin
+cls
+MODE con: COLS=43 lines=11
+echo.
+echo.          ===== 系统代理 =====
+echo.
+echo.   使用此功能前，务必先阅读使用说明！
+echo.
+echo.   --[1]--开启系统全局代理（IE代理）
+echo.   --[2]--取消系统全局代理
+echo.
+choice /c 12 /n /m "请选择【1-2】："
+
+echo %errorlevel%
+if %errorlevel% == 1 goto install
+if %errorlevel% == 2 goto uninstall
+
+
+
+::取消系统代理
+:uninstall
+cls
+
+taskkill /f /im iexplore.exe 1>nul 2>nul
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0 /f 1>nul 2>nul
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /d "" /f 1>nul 2>nul
+
+echo.
+echo.
+echo.
+echo.
+echo. 取消成功。
+ping localhost -n 2 1>nul 2>nul
+goto begin
+
+
+
+::配置系统代理
+:install
+cls
+
+taskkill /f /im iexplore.exe 1>nul 2>nul
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f 1>nul 2>nul
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /d "127.0.0.1:1087" /f 1>nul 2>nul
+
+echo.
+echo.
+echo.
+echo.
+echo. 开启成功。
+ping localhost -n 2 1>nul 2>nul
+goto begin
+
+
--- README.md
+++ README.md
@@ -4,13 +4,14 @@ C2ray 一个极简的 Project V 客户端
 
 ```
 极简 使用纯 BAT 编写 | 适用于 windows 系统
-绿色 开源 易于 DIY | 安静的在后台运行
+绿色  开源  易于 DIY | 安静的在后台运行
 ```
+
 使用需要对 V2ray 有一定的了解，无打扰后台静默运行 适合拥有长期稳定线路或者私人自建机场的老鸟使用。
 
-<img src="https://raw.githubusercontent.com/dylanbai8/c2ray/master/menu.jpg" width="50%">
+<img src="https://raw.githubusercontent.com/dylanbai8/c2ray/master/menu.jpg" width="40%">
 
-## 相关项目：
+## 关联项目：
 
 https://github.com/dylanbai8/c2ray_step_by_step
 
